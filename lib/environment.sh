a76f8878f9b32b13d6620b6533e37617cb7355f7
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-02-02 13:44:25 +0400

    fix: echo

--- lib/environment.sh
+++ lib/environment.sh
@@ -2,7 +2,6 @@ write_export() {
   local bp_dir="$1"
   local build_dir="$2"
   local cache_dir="$3"
-  local certs=""
-  for cert in $cache_dir; echo $cert; done
+  for cert in $cache_dir; echo "$cert"; done
   echo "export NODE_EXTRA_CA_CERTS=\"$certs\"" >> $bp_dir/export
 }
