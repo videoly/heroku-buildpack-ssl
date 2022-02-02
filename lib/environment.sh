429702fb8fc253355292199fbf000090f311a35f
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-02-02 13:48:01 +0400

    fix: pem

--- lib/environment.sh
+++ lib/environment.sh
@@ -2,6 +2,6 @@ write_export() {
   local bp_dir="$1"
   local build_dir="$2"
   local cache_dir="$3"
-  for cert in $cache_dir; do echo "$cert"; done
+  for cert in $cache_dir/*.pem; do echo "$cert"; done
   echo "export NODE_EXTRA_CA_CERTS=\"$certs\"" >> $bp_dir/export
 }
