2f0fe11f604fb32174d123659a3ff822b41dad68
Andrei Lapkin <andrew.lapkin.writer@gmail.com>
2022-02-02 13:49:24 +0400

    fix: try concat

--- lib/environment.sh
+++ lib/environment.sh
@@ -2,6 +2,7 @@ write_export() {
   local bp_dir="$1"
   local build_dir="$2"
   local cache_dir="$3"
-  for cert in $cache_dir/*.pem; do echo "$cert"; done
+  for cert in $cache_dir/*.pem; do certs="$certs $cert"; done
+  echo $certs
   echo "export NODE_EXTRA_CA_CERTS=\"$certs\"" >> $bp_dir/export
 }
